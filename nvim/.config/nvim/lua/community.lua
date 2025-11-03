4f70a79b2e864d31499e5209475ec5eac8983d90
Samuli Välimäki <samuli.valimaki@sitowise.com>
2025-11-03 09:43:02 +0200

    feat(config): new config with astro nvim

---
+++ nvim/.config/nvim/lua/community.lua
@@ -0,0 +1,28 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.comment.ts-comments-nvim" },
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  -- Languages
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.eslint" },
+  { import = "astrocommunity.pack.prettier" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.html-css" },
+  -- import/override with your plugins folder
+}
